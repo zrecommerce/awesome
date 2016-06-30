ac799705c7dd5fe9941fdb0ad5da2820bd0ff941
Alex Albino <webmaster@alexventure.com>
2016-06-29 22:47:30 -0700

    Now using material2 (angular2)

--- awesome-ux/src/app/app.component.ts
+++ awesome-ux/src/app/app.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
 
+import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav';
+import { MD_TOOLBAR_DIRECTIVES } from '@angular2-material/toolbar';
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
+// import { MD_CHECKBOX_DIRECTIVES } from '@angular2-material/checkbox';
+import { MD_RADIO_DIRECTIVES } from '@angular2-material/radio';
+import { MD_PROGRESS_CIRCLE_DIRECTIVES } from '@angular2-material/progress-circle';
+import { MD_PROGRESS_BAR_DIRECTIVES } from '@angular2-material/progress-bar';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
+import { MD_INPUT_DIRECTIVES } from '@angular2-material/input';
+import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
+import { MD_ICON_DIRECTIVES, MdIconRegistry } from '@angular2-material/icon';
+import { MD_TABS_DIRECTIVES } from '@angular2-material/tabs';
+//import { MdIcon, MdIconRegistry } from '@angular2-material/icon';
+
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [
+      MD_SIDENAV_DIRECTIVES,
+      MD_CARD_DIRECTIVES,
+      MD_TOOLBAR_DIRECTIVES,
+      MD_BUTTON_DIRECTIVES,
+      // MD_CHECKBOX_DIRECTIVES,
+      MD_RADIO_DIRECTIVES,
+      MD_PROGRESS_CIRCLE_DIRECTIVES,
+      MD_INPUT_DIRECTIVES,
+      MD_LIST_DIRECTIVES,
+      MD_PROGRESS_BAR_DIRECTIVES,
+      MD_ICON_DIRECTIVES,
+      MD_TABS_DIRECTIVES
+  ],
+  providers: [MdIconRegistry]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'It Works!';
 }
--- awesome-ux/src/system-config.ts
+++ awesome-ux/src/system-config.ts
@@ -7,11 +7,32 @@
  **********************************************************************************************/
 /** Map relative paths to URLs. */
 const map: any = {
+  '@angular2-material': 'vendor/@angular2-material'
 };
 
 /** User packages configuration. */
 const packages: any = {
 };
+
+const materialPkgs:string[] = [
+  'core',
+  'sidenav',
+  'toolbar',
+  'button',
+  'checkbox',
+  'radio',
+  'progress-circle',
+  'progress-bar',
+  'card',
+  'input',
+  'list',
+  'icon',
+  'tabs'
+];
+
+materialPkgs.forEach((pkg) => {
+  packages[`@angular2-material/${pkg}`] = {main: `${pkg}.js`};
+});
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 /***********************************************************************************************
